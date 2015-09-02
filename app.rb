0576209342b7f7519c2fed40471ec9664e1a7e6f
Sheva Goldberg <b@shevagoldberg.com>
2015-09-02 18:41:21 -0400

    Create Quote model

--- app.rb
+++ app.rb
@@ -3,6 +3,17 @@ require 'sinatra'
 require 'data_mapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/quotes.db")
+
+class Quote
+  include DataMapper::Resource
+  property :id, Serial
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  property :quote_text, Text, :required => true
+  property :quote_author, Text, :required => true, :default => 'NULL'
+end
+
 DataMapper.finalize
 DataMapper.auto_upgrade!
 get '/' do
