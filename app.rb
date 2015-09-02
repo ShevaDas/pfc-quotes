e294fc59520a14faec7373924dc6cfc68a1a5e9b
Sheva Goldberg <b@shevagoldberg.com>
2015-09-02 18:41:06 -0400

    Set up DataMapper

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
+# encoding: utf-8
 require 'sinatra'
+require 'data_mapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/quotes.db")
+DataMapper.finalize
+DataMapper.auto_upgrade!
 get '/' do
    "Hello World! Is it me you're looking for?"
 end
