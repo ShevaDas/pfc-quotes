9e8d50467a2e7acb706a65d929da3348cdfc7c76
Sheva Goldberg <b@shevagoldberg.com>
2015-09-03 01:35:49 -0400

    Handle error if quote id not found

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 require 'sinatra'
 require 'data_mapper'
+set :show_exceptions, :after_handler
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/quotes.db")
 
@@ -9,7 +10,6 @@ class Quote
   property :id, Serial
   property :created_at, DateTime
   property :updated_at, DateTime
-
   property :quote_text, Text, :required => true
   property :quote_author, Text, :required => true, :default => 'NULL'
 end
@@ -55,4 +55,8 @@ post '/quotes/?' do
   else
     "Something went wrong and your quote wasn't saved. Try again?"
   end
+end
+
+error DataMapper::ObjectNotFoundError do
+  haml :not_found
 end
